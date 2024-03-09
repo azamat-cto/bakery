38eb1bae60d6bbbd8ae6f58237f19e0811b482db
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-03-09 16:02:59 +0500

    Add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 function Home() {
-    return <div>App</div>;
+    return (
+        <div className="flex items-center justify-center h-[100dvh]">
+            <h1 className="text-3xl font-bold underline">App</h1>
+        </div>
+    );
 }
 
 export default Home;
