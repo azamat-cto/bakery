6e70ca0b4bf183bf3d18aec3fe96e554713991ce
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-03-09 21:29:19 +0500

    Add prettier eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,41 @@
 import "@/styles/globals.css";
 
-import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
+import type { ReactNode } from "react";
+
 import { cn } from "@/lib/utils";
 
 type RootLayoutProps = {
-    children: ReactNode;
+	children: ReactNode;
 };
 
 const fontSans = FontSans({
-    subsets: ["latin"],
-    variable: "--font-sans",
+	subsets: ["latin"],
+	variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-    title: "",
-    description: "",
+	title: "",
+	description: "",
 };
 
 function RootLayout({ children }: Readonly<RootLayoutProps>) {
-    return (
-        <html
-            lang="en"
-            suppressHydrationWarning
-            className={cn(fontSans.variable)}
-        >
-            <body
-                className={cn(
-                    "bg-background text-foreground font-sans antialiased"
-                )}
-            >
-                {children}
-            </body>
-        </html>
-    );
+	return (
+		<html
+			lang="en"
+			suppressHydrationWarning
+			className={cn(fontSans.variable)}
+		>
+			<body
+				className={cn(
+					"bg-background text-foreground font-sans antialiased",
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
 
 export default RootLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 
 function Home() {
-    return (
-        <div className="flex items-center justify-center h-[100dvh]">
-            <Button>Button</Button>
-        </div>
-    );
+	return (
+		<div className="flex h-[100dvh] items-center justify-center">
+			<Button>Button</Button>
+		</div>
+	);
 }
 
 export default Home;
