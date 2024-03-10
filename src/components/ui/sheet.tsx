8e7b6c49edae1c59a704eb0b0b4efdc67bdfe9d0
azamatcto <azamatkhabibullaevcto@gmail.com>
2024-03-10 19:45:22 +0500

    Add sorting imports

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -5,8 +5,8 @@ import { Cross2Icon } from "@radix-ui/react-icons";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const Sheet = SheetPrimitive.Root;
 
